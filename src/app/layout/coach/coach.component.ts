156d10602751423b65ba3fd22e16373ea4324ad7
Sergio Arellano Diaz <sergioarellanodiaz@gmail.com>
2018-07-24 18:04:04 +0200

    Validazione formulario allenatore

--- src/app/layout/coach/coach.component.ts
+++ src/app/layout/coach/coach.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-coach',
     templateUrl: './coach.component.html',
-    styleUrls: ['./coach.component.scss']
+    styleUrls: ['./coach.component.scss'],
+    providers: [ ]
 })
 export class CoachComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+    inserimentoFormAllenatore: string = 'Inserimento Allenatore';
+    form: FormGroup;
+    submitted = false;
+    constructor(private formBuilder: FormBuilder) {}
+    ngOnInit() {
+       this.form = this.formBuilder.group({
+          tipoLicenza: [null, Validators.required],
+          nome: [null, Validators.required],
+          cognome: [null, Validators.required],
+          regione: [null, Validators.required],
+          provincia: [null, Validators.required],
+          comune: [null, Validators.required],
+          email: [null, [Validators.required, Validators.email]]
+    }  );
+    }
+    get f() { return this.form.controls; }
+    inviaRegistrazione(event) {
+       this.submitted = true;
+       if (this.form.valid) {
+          console.log('form submitted');
+      } else {         
+          console.log(this.form);
+      }
+    }
 }
--- src/app/layout/coach/coach.module.ts
+++ src/app/layout/coach/coach.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CoachRoutingModule } from './coach-routing.module';
 import { CoachComponent } from './coach.component';
 
+//import { SimpleFormComponent } from './simple-form/simple-form.component';
+//import { SubmitFlagFormComponent } from './submit-flag-form/submit-flag-form.component';
+//import { ValidateFieldsSubmitFormComponent } from './validate-fields-submit-form/validate-fields-submit-form.component';
+//import { FieldErrorDisplayComponent } from './field-error-display/field-error-display.component';
+
 @NgModule({
-    imports: [CommonModule, CoachRoutingModule],
+    imports: [CommonModule, CoachRoutingModule,
+        ReactiveFormsModule],
     declarations: [CoachComponent]
 })
 export class CoachModule {}
